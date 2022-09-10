6aba0506470a745aabd97b9173a3226be8227ac8
victoriababy45 <vickkykwosah@gmail.com>
2022-09-10 07:24:33 -0400

    added stages to victoriaapp2.groovyfile

---
+++ vars/victoriaapp2.groovy
@@ -0,0 +1,39 @@
+def call(String repoUrl){
+pipeline {
+       agent any
+       tools {
+           maven 'maven'
+       }
+       stages {
+           stage("Tools initialization") {
+               steps {
+                   sh 'mvn --version'
+                   sh 'java -version'
+               }
+           }
+           stage('demo-newapp'){
+           steps{
+           sh 'cat /etc/passwd'
+           echo "jenkins is funs"
+           }
+           }
+           stage('demo-newapp'){
+           steps{
+                sh 'cat/etc/passwd'
+                echo "jenkins was an esay module"
+           }
+           }
+           stage("Checkout Code") {
+               steps {
+                   git branch: 'main',
+                          url: "${repoUrl}"
+               }
+           }
+           stage("to-test-maven") {
+               steps {
+                   sh 'mvn -v'
+               }
+           }
+       }
+}
+}
